738a95d8ae5fc84d06d5f247c52738051406fda9
Minjung Bin <minjungbin@gmail.com>
2020-02-24 16:13:26 -0500

    added initial typography page

---
+++ ds-ux-guidelines-gatsby/src/pages/foundation/typography.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Layout from "../../ds-components/layout/layout"
+import ContentHeader from "../../ds-components/content-header/contenHeader"
+import SEO from "../../ds-components/seo"
+
+import '../../../../ds-css/src/styles/cbp-ds.scss';
+
+const TypographyPage = () => (
+  <Layout>
+    <SEO title="CBP Design System | Typography" />
+    <ContentHeader title="Typography" />
+
+    <article>
+      <p>
+        Having a well thought-out and managed typography system drives retention
+        and understanding of information, and helps our users make the informed
+        decisions that keep our country safe. This isn't simply what typeface we
+        would like to use, but also the hierarchy, treatments, spacial
+        relationships, and other refinements which augment legibility.
+        <br /> <br />
+        At its core, values such as consistency, accessibility, and legibility
+        should be valued above all others.
+      </p>
+    </article>
+  </Layout>
+)
+
+export default TypographyPage
